19689cb2f60e1aca9b4567d3bc5b4a051a58c20e
athylaguilherme <athylaguilherme20@gmail.com>
2022-08-11 22:26:34 -0300

    bug

---
+++ Pizzaria/Data/PizzariaDbContext.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Pizzaria.Models;
+
+namespace IngressoMVC.Data
+{
+    public class PizzariaDbContext : DbContext
+    {
+        public PizzariaDbContext(DbContextOptions<PizzariaDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PizzaSabor>().HasKey(pizzaSabor => new
+            {
+                pizzaSabor.PizzaId,
+                pizzaSabor.SaborId
+            });
+
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<PizzaSabor> PizzasSabores { get; set; }
+        public DbSet<Sabor> Sabores { get; set; }
+        public DbSet<Tamanho> Tamanhos { get; set; }
+    }
+}
--- Pizzaria/Startup.cs
+++ Pizzaria/Startup.cs
@@ -1,6 +1,8 @@
+using IngressoMVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,7 @@ namespace Pizzaria
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<PizzariaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
